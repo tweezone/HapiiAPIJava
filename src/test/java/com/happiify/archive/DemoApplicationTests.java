79783d4032231e5aefbef868f62c55ab56d46104
Frank Yan <frank.v.yan@gmail.com>
2018-12-03 16:59:01 -0500

    file encryption function

--- src/test/java/com/happiify/archive/DemoApplicationTests.java
+++ src/test/java/com/happiify/archive/DemoApplicationTests.java
@@ -24,82 +24,15 @@ public class DemoApplicationTests {
 
 
 	}
-	@Test
-	public void myTest(){
-		String path = "/Users/chenguoyan/Desktop/uploadedfile/";
-
-		try {
-			AESEncrypter aes = new AESEncrypter("hello89078678");
-			//String base64FromFile = fileToBase64(path+"test.txt");
-
-			File file = new File(path, "test.txt");
-			InputStream in = new FileInputStream(file);
-			byte[] bytes = new byte[in.available()];
-
-			String readStr = new String(bytes,"utf-8");
-
-			String base64Str = Base64.getEncoder().encodeToString(bytes);
-			//String plainStr = new String(plainData);
-			byte[] plainData = Base64.getDecoder().decode(base64Str);
-
-			String finalStr = new String(plainData);
-
-			System.out.println(base64Str);
 
 
-//			String test = "星期六123Abc";
-//			byte[] encrypted = aes.encrypt(test.getBytes());
-//			byte[] decrypted = aes.decrypt(encrypted);
-//
-//			System.out.println(new String(decrypted));
 
-			//encryptFile(path,"test.txt","hello89078678");
-			decryptFile(path,"test.txt","hello89078678");
-
-		} catch (Exception e) {
-			System.out.println("something wrong");
-			e.printStackTrace();
-		}
-		System.out.println("this is test");
-	}
-
-	void decryptFile(String filePath, String fileName, String password) {
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-		File file = new File(filePath,fileName);
-		try {
-			inputStream = new FileInputStream(file);
-			byte[] data = new byte[inputStream.available()];
-			AESEncrypter aesEncrypter = new AESEncrypter(password);
-			String  encryptedContent = new String(data);
-
-			String decryptedContent = aesEncrypter.decrypt(encryptedContent);
-			//byte[] originalContent = Base64.getDecoder().decode(decryptedContent);
-
-			outputStream = new FileOutputStream(file,false);
-			outputStream.write(decryptedContent.getBytes());
-			outputStream.flush();
-			outputStream.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
 
 	void encryptFile(String filePath, String fileName, String password) {
 		FileOutputStream fileOutputStream = null;
 		String base64Content = fileToBase64(filePath + fileName);
 		System.out.println(base64Content);
 		try {
-			AESEncrypter aesEncrypter = new AESEncrypter(password);
-			String encryptedContent = aesEncrypter.encrypt(base64Content);
-			File file = new File(filePath, fileName);
-			fileOutputStream = new FileOutputStream(file, false);
-			byte[] data = encryptedContent.getBytes();
-			fileOutputStream.write(data);
-			fileOutputStream.flush();
-			fileOutputStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
